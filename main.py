54ca986e14b8e34a9224f8bb015393e486e7aba2
Aranz44rmas <aranzaarmasromero@gmail.com>
2025-07-08 05:15:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
+# src/main.py
+
+# Import necessary libraries
 import json
 import logging
 import os
 from dotenv import load_dotenv
+
+# Import custom modules from the project structure
 from src.aurora_extractor import AuroraProjectDataExtractor
 from src.utils.logger import setup_logging # Importar la función de configuración de logging
 
-# Configurar logging al inicio de la aplicación
+# Configure logging at the application start
 setup_logging()
 logger = logging.getLogger(__name__)
 
-base_src_path = os.path.dirname(__file__) # Esto es 'src/'
+ # Open the auth.json file in read mode with UTF-8 encoding and load the JSON content into a Python dictionary
+base_src_path = os.path.dirname(__file__) 
 auth_json_file_path = os.path.join(base_src_path, 'config', 'auth.json')
+
 try:
   with open(auth_json_file_path, 'r', encoding='utf-8') as f_auth_json:
     config_data = json.load(f_auth_json)
-    logger.info(f"✅ Datos de autenticación cargados exitosamente desde src/config/auth.json: {auth_data}")
+    logger.info(f"✅ Auth data successfully loaded from src/config/auth.json: {auth_data}")
 except FileNotFoundError:
-  logger.warning(f"⚠️ El archivo de autenticación no se encontró en: {auth_json_file_path}")
+  logger.warning(f"⚠️ Auth file not found at: {auth_json_file_path}")
 except json.JSONDecodeError:
-  logger.error(f"❌ El archivo '{auth_json_file_path}' no es un JSON válido.")
+  logger.error(f"❌ File '{auth_json_file_path}' is not a valid JSON .")
 except Exception as e:
-  logger.error(f"❌ Error al cargar el archivo de autenticación: {e}")
+  logger.error(f"❌ Error loading auth file: {e}")
 
-
+# Set Authentication and Project Variables
 tenant_id = config_data['tenant_id']
 bearer_token = config_data['bearer_token']
 auth = config_data['auth']
@@ -31,12 +38,15 @@ project_ids = config_data['project_ids']
 
 fields_to_return = [6,7,8,9,10,12,13,14,15,16,17,18,19]
 
-# Ejecutar publicación para cada proyecto
+# Execute post for every JSON project_id's
+
+# Process each projectr project in project_ids:
 for project in project_ids:
     project_id = project['id']
     label = project.get('label', project_id)
     print(f"\n🔄 Procesando {label} ({project_id})")
 
+    # Initialize the extractor for the current project
     try:
         extractor = AuroraProjectDataExtractor(tenant_id, project_id, bearer_token)
         final_df = extractor.extract_all_data()
@@ -45,6 +55,7 @@ for project in project_ids:
             print(f"⚠️ No data extracted for {label}. Skipping.")
             continue
 
+        # Format and publish in Quickbase
         payload = extractor.format_for_quickbase(final_df, table_id, return_fields=fields_to_return)
         result = extractor.post_to_quickbase(payload, user_token=auth)
 
